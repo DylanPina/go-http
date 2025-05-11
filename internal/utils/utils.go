534357711b36776b8cef3356a97a049de847a18f
Dylan Pina <dylansp.dev@gmail.com>
2025-05-11 14:27:00 -0500

    Added Gzip utils

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,7 +1,10 @@
 package utils
 
 import (
+	"bytes"
+	"compress/gzip"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 )
@@ -23,4 +26,30 @@ func FindProjectRoot() (string, error) {
 		}
 		dir = parent
 	}
+}
+
+func GzipCompress(data []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	writer := gzip.NewWriter(&buf)
+
+	_, err := writer.Write(data)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := writer.Close(); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func GzipDecompress(data []byte) ([]byte, error) {
+	reader, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	return io.ReadAll(reader)
 }
